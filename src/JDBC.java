8f03ee01f689c7bba25089ecf7067232293dd2ff
Sabrina Sumona <snsbauet04@gmail.com>
2021-07-08 13:18:34 +0600

    Project upload

---
+++ src/JDBC.java
@@ -0,0 +1,44 @@
+import java.sql.*;
+
+public class JDBC {
+    private Connection connection;
+    private PreparedStatement preparedStatement;
+    private ResultSet resultSet;
+
+    private final String url = "jdbc:mysql://localhost:3306/students";
+    private final String username = "sabrina_sumona";
+    private final String password = "sns963147";
+
+    public Connection getConnection() {
+        Connection con = null;
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            con = DriverManager.getConnection(url, username, password);
+        } catch (ClassNotFoundException | SQLException e) {
+            e.printStackTrace();
+        }
+        return con;
+    }
+
+    public void insertStudent(int std_id, String std_name, String department, int batch) throws SQLException {
+        this.connection = this.getConnection();
+
+        this.preparedStatement = this.connection.prepareStatement(
+                "INSERT INTO student_info (std_id, std_name, department, batch) VALUES (?,?,?,?)"
+        );
+
+        //        here 1st '?' will be replaced by value of index 1, then 2nd one by index 2 and so on....
+        this.preparedStatement.setInt(1, std_id);
+        this.preparedStatement.setString(2, std_name);
+        this.preparedStatement.setString(3, department);
+        this.preparedStatement.setInt(4, batch);
+
+        int affectedRows = this.preparedStatement.executeUpdate();
+        System.out.println("-----------------------------------------------");
+        System.out.println(affectedRows + " student's data has inserted into students database...");
+
+        this.connection.close();
+        this.preparedStatement.close();
+    }
+
+}
---
+++ src/JDBC_with_GUI.java
@@ -0,0 +1,53 @@
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.sql.SQLException;
+
+public class JDBC_with_GUI extends JFrame {
+    private JPanel mainPanel;
+    private JLabel stdid_label;
+    private JLabel stdname_label;
+    private JLabel stddept_label;
+    private JLabel stdbtc_label;
+    private JTextField std_id;
+    private JTextField std_name;
+    private JTextField std_dept;
+    private JTextField std_btc;
+    private JButton ok;
+    private JLabel response;
+    private JLabel info;
+
+    public JDBC_with_GUI(String title){
+        super(title);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setContentPane(mainPanel);
+        this.pack();
+        ok.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                JDBC dbApp = new JDBC();
+                try {
+                    dbApp.insertStudent(
+                            Integer.parseInt(std_id.getText()),
+                            std_name.getText(),
+                            std_dept.getText(),
+                            Integer.parseInt(std_btc.getText())
+                    );
+                } catch (SQLException troubles) {
+                    troubles.printStackTrace();
+                }
+                response.setText("1 Student Info has been recorded ::: ");
+                info.setText("ID: "+std_id.getText()+", name: "+std_name.getText()+", Dept.: "+std_dept.getText()+", Batch: "+std_btc.getText());
+                std_id.setText("");
+                std_name.setText("");
+                std_dept.setText("");
+                std_btc.setText("");
+            }
+        });
+    }
+    public static void main(String[] args) {
+        JFrame frame = new JDBC_with_GUI("Student Info");
+        frame.setVisible(true);
+        frame.setSize(700,350);
+    }
+}
